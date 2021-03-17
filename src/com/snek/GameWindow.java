f1afe90fd47d3e9a067e84bfca251ed974aa1266
kylest0kes <kylestokes1551@gmail.com>
2021-03-17 16:51:23 -0400

    built constructor for main class and game panel. window pops up when program is ran

---
+++ src/com/snek/GameWindow.java
@@ -0,0 +1,18 @@
+package com.snek;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWindow extends JFrame {
+
+    GameWindow() {
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
--- src/com/snek/SnakeGame.java
+++ src/com/snek/SnakeGame.java
@@ -1,8 +1,8 @@
-package com.kyle;
+package com.snek;
 
 public class SnakeGame {
 
     public static void main(String[] args) {
-	// write your code here
+        new GameWindow();
     }
 }
